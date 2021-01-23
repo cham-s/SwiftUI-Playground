b18fe94ca6364cb4a62ec7daf3ffd89e539b8aea
cham-s <cc.attou@gmail.com>
2021-01-23 18:41:28 +0100

    adding clock project

---
+++ Clock/Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by chams on 23/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
